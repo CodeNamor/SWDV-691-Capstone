6418d0823c5837ec5fe1ab234a978b6f3087fafe
F Derek Roman <derek.roman81@gmail.com>
2019-08-01 22:49:46 -0500

    updating service. tab1.page.ts has errors to rslv

--- src/app/movie-service.service.ts
+++ src/app/movie-service.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { map, delay } from 'rxjs/operators';
 import { Movie } from '../models/movie';
 import {  } from 'q';
