62fb0f2b1e41fb9cd532823a5d811a22a5a4cf43
F Derek Roman <derek.roman81@gmail.com>
2019-08-21 23:05:01 -0500

    add CRUD funcs to database on profile.page.ts file

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { StorageService, Item } from '../../services/storage.service';
+import { ToastController, IonList, Platform } from '@ionic/angular';
 import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
@@ -12,7 +14,18 @@ export class ProfilePage implements OnInit {
   submitted = false;
   profileForm: FormGroup;
 
-  constructor(private router: Router, private formBuilder: FormBuilder) { }
+  items: Item[] = [];
+ 
+  newItem: Item = <Item>{};
+ 
+  @ViewChild('mylist', {static: false})mylist: IonList;
+
+  constructor(private router: Router, private formBuilder: FormBuilder, private storageService: StorageService, 
+    private toastController: ToastController, private plt: Platform) {
+      this.plt.ready().then(() => {
+        this.loadItems();
+      });
+    }
 
   ngOnInit() {
     this.profileForm = this.formBuilder.group({
@@ -24,6 +37,55 @@ export class ProfilePage implements OnInit {
         favMovie: [''],
         favAct: ['']
     }, {});
+}
+
+// CREATE
+addItem() {
+  this.newItem.modified = Date.now();
+  this.newItem.id = Date.now();
+
+  this.storageService.addItem(this.newItem).then(item => {
+    this.newItem = <Item>{};
+    this.showToast('Item added!')
+    this.loadItems(); // Or add it to the array directly
+  });
+}
+
+// READ
+loadItems() {
+  this.storageService.getItems().then(items => {
+    this.items = items;
+  });
+}
+
+// UPDATE
+updateItem(item: Item) {
+  item.title = `UPDATED: ${item.title}`;
+  item.modified = Date.now();
+
+  this.storageService.updateItem(item).then(item => {
+    this.showToast('Item updated!');
+    this.mylist.closeSlidingItems(); // Fix or sliding is stuck afterwards
+    this.loadItems(); // Or update it inside the array directly
+  });
+}
+
+// DELETE
+deleteItem(item: Item) {
+  this.storageService.deleteItem(item.id).then(item => {
+    this.showToast('Item removed!');
+    this.mylist.closeSlidingItems(); // Fix or sliding is stuck afterwards
+    this.loadItems(); // Or splice it from the array directly
+  });
+}
+
+// Helper
+async showToast(msg) {
+  const toast = await this.toastController.create({
+    message: msg,
+    duration: 2000
+  });
+  toast.present();
 }
 
 }
