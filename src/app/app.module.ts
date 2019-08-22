c9b289e7fd2152b75f1c5ddf87d9bccb8d924365
F Derek Roman <derek.roman81@gmail.com>
2019-08-21 22:49:34 -0500

    Modification to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,20 +10,21 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { SQLite } from '@ionic-native/sqlite/ngx';
+
 import { HttpClientModule } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule, IonicStorageModule.forRoot()],
   exports: [
     FormsModule,
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    SQLite,
+
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
